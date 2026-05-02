b512c9a1b1a4ec1d14e2181521d15133f0deb1d1
murielRamello <murivento@gmail.com>
2026-05-01 22:57:15 -0300

    modificación NavBAr

---
+++ src/components/Footer/Footer.componen.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            <h3>ESTE ES EL FOOTER</h3>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/NavBar/NavBar.component.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styles from "./NavBar.module.css";
+
+const Navbar = () => {
+    return (
+        <nav className={styles.navbar}>
+            <h3>Esta es la NavBar</h3>
+        </nav>
+    )
+}
+
+export default Navbar
