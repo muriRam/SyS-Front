84297ed70a9bab86c24cc8717df292862c63c539
murielRamello <murivento@gmail.com>
2026-05-14 15:01:22 -0300

    fix: arreglo de nombre de footer.component.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/NavBar/NavBar.component";
-import Footer from "@/components/Footer/Footer.componen";
+import Footer from "@/components/Footer/Footer.component";
 
 const inter = Inter({
   subsets: ["latin"],
