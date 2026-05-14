a33856953f2aaba255231a5b3cd5d451c2efac19
murielRamello <murivento@gmail.com>
2026-05-14 19:40:10 -0300

    fix: correccion de layout + HomePage

--- src/components/pages/home/HomePage.component.tsx
+++ src/components/pages/home/HomePage.component.tsx
@@ -1,11 +1,11 @@
 import styles from "./HomePage.module.css";
 
 export const HomePage = () => (
-    <div className={`flex-1 ${styles.main}`}>
-        <section className={styles.section}>
+    <div className={styles.container}>
+        <section className={styles.searchSection}>
             <h1> Aca va la search var</h1>
         </section>
-        <section className={styles.section}>
+        <section className={styles.templatesSection}>
             <h2> aca deberian estar peque;as vistas que funcionen como accesos directos a los templates</h2>
         </section>
     </div>
